7531587aa63ed9163c8087ea1364a74a2ab95be5
LorenzoBaracchi <baracchi.lorenzo@gmail.com>
2018-01-19 15:46:34 +0100

    group cities by country

--- app/main.rb
+++ app/main.rb
@@ -1,7 +1,12 @@
+require_relative 'city.rb'
+
 module Main
 
   def self.csv_to_city csv_line
     City.new(name: csv_line[2], country: csv_line[1], altitude: csv_line[5])
   end
 
+  def self.group_by cities, attribute
+    cities.group_by {|city| city.send(attribute)}
+  end
 end
